369a8c91ca121ea7938a77f65dd230592879ab4f
Arpitha93 <arpitha1993@gmail.com>
2016-03-09 00:08:03 +0530

    Added files via upload

---
+++ StringClass.java
@@ -0,0 +1,19 @@
+package string;
+
+public class StringClass {
+
+	public static void main(String[] args) {
+		String s1="arpitha";
+		  
+		char ch[]={'n','a','g','a','v','a','r','a'};  
+		String s2=new String(ch);  
+		  
+		String s3=new String("ananthamurthy");  
+		  
+		System.out.println(s1);  
+		System.out.println(s2);  
+		System.out.println(s3);  
+
+	}
+
+}
