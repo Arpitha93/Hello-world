790ae428cd1dca959e3f91495072e275d0d34d44
Arpitha93 <arpitha1993@gmail.com>
2016-05-21 15:21:15 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        double y=sc.nextDouble();
-        sc.nextLine();
-        String s=sc.nextLine();
-         
+package stringCompare;
 
-        System.out.println("String: "+s);
-        System.out.println("Double: "+y);
-        System.out.println("Int: "+x);
-}
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        String A = sc.next();
+        int n = sc.nextInt();
+        String max = A.substring(0,n);
+        String min = A.substring(0,n);
+        for(int i=0; i+n<=A.length(); i++)
+            {
+           if(A.substring(i,i+n).compareTo(min)<0) min=A.substring(i,i+n);
+           if(A.substring(i,i+n).compareTo(max)>0) max=A.substring(i,i+n);
+        }
+            
+                System.out.println(min);
+                System.out.println(max);
+            
+	}
+
 }
