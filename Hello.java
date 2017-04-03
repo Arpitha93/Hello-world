ee7995db74d42ec3ca444ceeb358d9b475f8a5b4
Arpitha93 <arpitha1993@gmail.com>
2017-04-03 12:32:56 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Arpitha on 20-03-17.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        int myFirstNumber = (10+15) + (2*10);
+        int mySecondNumber = 12;
+        int myThridNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThridNumber;
+        System.out.println(myTotal);
+    }
+}
