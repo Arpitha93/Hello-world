04334b58cb245861f9652d0fbf9a3b36c2adbb48
Arpitha93 <arpitha1993@gmail.com>
2017-05-19 13:17:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,11 +3,8 @@ package com.arpitha;
 public class Main {
 
     public static void main(String[] args) {
-	int myIntValue = 5/3;
-	float myFloatValue = 5f/3f;
-	double myDoubleValue = 5d/3d;
-        System.out.println("myIntValue =" +myIntValue);
-        System.out.println("myFLoatValue=" +myFloatValue);
-        System.out.println("mydoublevalue =" +myDoubleValue);
+	double myPounds = 200d;
+	double convertKg = myPounds * 0.45359237;
+        System.out.println(" converted kilograms =" +convertKg);
     }
 }
